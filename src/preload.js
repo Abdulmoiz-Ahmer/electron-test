f2378837193cdb0c9cb5daf63dd80327d7a19244
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2023-02-28 22:40:55 +0500

    Test

---
+++ src/preload.js
@@ -0,0 +1,6 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("codeInput").focus();
+});
